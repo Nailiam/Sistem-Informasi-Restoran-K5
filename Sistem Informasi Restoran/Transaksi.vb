170988f425a74fb87e62307815eb97930ef2ead8
Nadia1944 <63362943+Nadia1944@users.noreply.github.com>
2020-12-17 05:53:04 +0700

    jml+items +=nadia, load +kosongkan item = edhi

--- Sistem Informasi Restoran/Transaksi.vb
+++ Sistem Informasi Restoran/Transaksi.vb
@@ -15,9 +15,8 @@
                 txtjumlah.Enabled = True
                 txtjumlah.Focus()
             End If
+        End If
     End Sub
-
-
     Sub RumusGrandTotal()
         Dim hitung As Integer = 0
         For i As Integer = 0 To DataGridView1.Rows.Count - 1
@@ -29,9 +28,8 @@
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
-    Private Sub txtbayar_keypress(ByVal sender As Object, ByVal e As
-System.Windows.Forms.KeyPressEventArgs) Handles txtbayar.KeyPress
- If e.KeyChar = Chr(13) Then
+    Private Sub txtbayar_keypress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtbayar.KeyPress
+        If e.KeyChar = Chr(13) Then
             If Val(txtbayar.Text) < Val(txtGrandtotal.Text) Then
                 MsgBox("Uang Pembayaran Kurang")
             ElseIf Val(txtbayar.Text) = Val(txtGrandtotal.Text) Then
@@ -43,9 +41,8 @@ System.Windows.Forms.KeyPressEventArgs) Handles txtbayar.KeyPress
         End If
     End Sub
 
-    Private Sub btn_simpan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-Handles btn_simpan.Click
- If txtkembali.Text = "" Or txtGrandtotal.Text = "" Then
+    Private Sub btn_simpan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_simpan.Click
+        If txtkembali.Text = "" Or txtGrandtotal.Text = "" Then
             MsgBox("Data Transaksi Belum Lengkap")
             'Pengecekan , apabila transaksi belum terjadi maka tidak bisa di ENTER
         Else
@@ -82,6 +79,38 @@ DataGridView1.Rows(baris).Cells(0).Value & "'"
 
     Private Sub txtbayar_TextChanged(sender As Object, e As EventArgs) Handles txtbayar.TextChanged
 
+    End Sub
+    Sub carijumlahitem()
+        Dim hitungitem As Integer = 0
+        For i As Integer = 0 To DataGridView1.Rows.Count - 1
+            hitungitem = hitungitem + DataGridView1.Rows(i).Cells(3).Value
+            txtitems.Text = hitungitem
+        Next
+    End Sub
+    Sub kosongkanitem()
+        txtkodestruk.Text = ""
+        txtidpelanggan.Text = ""
+        txtnamapelanggan.Text = ""
+        txtnomeja.Text = ""
+    End Sub
+    Private Sub txtjumlah_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtjumlah.KeyPress
+        If e.KeyChar = Chr(13) Then
+            If txtnama.Text = "" Or txtharga.Text = "" Then
+                MsgBox("Masukkan Kode Kode Barang dan tekan enter ")
+            Else
+                DataGridView1.Rows.Add(New String() {txt_Kodepesanan.Text,
+txtnama.Text, txtharga.Text, txtjumlah.Text, Val(txtharga.Text) *
+Val(txtjumlah.Text)})
+                Call RumusGrandTotal()
+                Call kosongkanitem()
+                txt_Kodepesanan.Focus()
+                Call carijumlahitem()
+            End If
+        End If
+    End Sub
+    Private Sub TransaksiPenjualan_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call kondisiawal()
+        Call Nomorfakturotomatis()
     End Sub
 End Class
 
