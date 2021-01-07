d6089dda4af413c85ab53684a7d1505692dcdf2e
rahayuskrn06 <rahayuskrn0600@gmail.com>
2021-01-06 20:23:10 -1200

    beberapa perubahan

--- Sistem Informasi Restoran/Form1.Designer.vb
+++ Sistem Informasi Restoran/Form1.Designer.vb
@@ -38,105 +38,95 @@ Partial Class Form1
         '
         'btnsavebarcode
         '
-        Me.btnsavebarcode.Location = New System.Drawing.Point(618, 361)
-        Me.btnsavebarcode.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnsavebarcode.Location = New System.Drawing.Point(464, 293)
         Me.btnsavebarcode.Name = "btnsavebarcode"
-        Me.btnsavebarcode.Size = New System.Drawing.Size(100, 28)
+        Me.btnsavebarcode.Size = New System.Drawing.Size(75, 23)
         Me.btnsavebarcode.TabIndex = 19
         Me.btnsavebarcode.Text = "Save"
         Me.btnsavebarcode.UseVisualStyleBackColor = True
         '
         'btnsaveqrcode
         '
-        Me.btnsaveqrcode.Location = New System.Drawing.Point(65, 361)
-        Me.btnsaveqrcode.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnsaveqrcode.Location = New System.Drawing.Point(49, 293)
         Me.btnsaveqrcode.Name = "btnsaveqrcode"
-        Me.btnsaveqrcode.Size = New System.Drawing.Size(100, 28)
+        Me.btnsaveqrcode.Size = New System.Drawing.Size(75, 23)
         Me.btnsaveqrcode.TabIndex = 18
         Me.btnsaveqrcode.Text = "Save"
         Me.btnsaveqrcode.UseVisualStyleBackColor = True
         '
         'PictureBox2
         '
-        Me.PictureBox2.Location = New System.Drawing.Point(539, 170)
-        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox2.Location = New System.Drawing.Point(404, 138)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(248, 148)
+        Me.PictureBox2.Size = New System.Drawing.Size(186, 120)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox2.TabIndex = 17
         Me.PictureBox2.TabStop = False
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(18, 170)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Location = New System.Drawing.Point(14, 138)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(212, 148)
+        Me.PictureBox1.Size = New System.Drawing.Size(159, 120)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 16
         Me.PictureBox1.TabStop = False
         '
         'btnprosesbarcode
         '
-        Me.btnprosesbarcode.Location = New System.Drawing.Point(586, 112)
-        Me.btnprosesbarcode.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnprosesbarcode.Location = New System.Drawing.Point(440, 91)
         Me.btnprosesbarcode.Name = "btnprosesbarcode"
-        Me.btnprosesbarcode.Size = New System.Drawing.Size(100, 28)
+        Me.btnprosesbarcode.Size = New System.Drawing.Size(75, 23)
         Me.btnprosesbarcode.TabIndex = 15
         Me.btnprosesbarcode.Text = "Proses"
         Me.btnprosesbarcode.UseVisualStyleBackColor = True
         '
         'btnprosesqrcode
         '
-        Me.btnprosesqrcode.Location = New System.Drawing.Point(65, 112)
-        Me.btnprosesqrcode.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnprosesqrcode.Location = New System.Drawing.Point(49, 91)
         Me.btnprosesqrcode.Name = "btnprosesqrcode"
-        Me.btnprosesqrcode.Size = New System.Drawing.Size(100, 28)
+        Me.btnprosesqrcode.Size = New System.Drawing.Size(75, 23)
         Me.btnprosesqrcode.TabIndex = 14
         Me.btnprosesqrcode.Text = "Proses"
         Me.btnprosesqrcode.UseVisualStyleBackColor = True
         '
         'txtbarcode
         '
-        Me.txtbarcode.Location = New System.Drawing.Point(539, 80)
-        Me.txtbarcode.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtbarcode.Location = New System.Drawing.Point(404, 65)
         Me.txtbarcode.Name = "txtbarcode"
-        Me.txtbarcode.Size = New System.Drawing.Size(132, 22)
+        Me.txtbarcode.Size = New System.Drawing.Size(100, 20)
         Me.txtbarcode.TabIndex = 13
         '
         'txtqrcode
         '
-        Me.txtqrcode.Location = New System.Drawing.Point(18, 80)
-        Me.txtqrcode.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtqrcode.Location = New System.Drawing.Point(14, 65)
         Me.txtqrcode.Name = "txtqrcode"
-        Me.txtqrcode.Size = New System.Drawing.Size(132, 22)
+        Me.txtqrcode.Size = New System.Drawing.Size(100, 20)
         Me.txtqrcode.TabIndex = 12
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(535, 61)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Location = New System.Drawing.Point(401, 50)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(61, 17)
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
         Me.Label2.TabIndex = 11
         Me.Label2.Text = "Barcode"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(14, 61)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(10, 50)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(66, 17)
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
         Me.Label1.TabIndex = 10
         Me.Label1.Text = "QR Code"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(965, 450)
+        Me.ClientSize = New System.Drawing.Size(724, 366)
         Me.Controls.Add(Me.btnsavebarcode)
         Me.Controls.Add(Me.btnsaveqrcode)
         Me.Controls.Add(Me.PictureBox2)
@@ -147,6 +137,7 @@ Partial Class Form1
         Me.Controls.Add(Me.txtqrcode)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
--- Sistem Informasi Restoran/Form1.vb
+++ Sistem Informasi Restoran/Form1.vb
@@ -48,4 +48,8 @@ Public Class Form1
             End Try
         End If
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
