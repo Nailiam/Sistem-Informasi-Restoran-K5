65c064285e852d2c1fa99046ed32bdd97d9a6187
Nadia1944 <63362943+Nadia1944@users.noreply.github.com>
2021-01-15 13:44:36 +0700

    Form report viewer id card

--- Sistem Informasi Restoran/Crystal Report ID Card.vb
+++ Sistem Informasi Restoran/Crystal Report ID Card.vb
@@ -91,6 +91,14 @@ Public Class Crystal_Report_ID_Card
             Return Me.ReportDefinition.Sections(4)
         End Get
     End Property
+    
+    <Browsable(false),  _
+     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
+    Public ReadOnly Property Parameter_ID_Kasir() As CrystalDecisions.[Shared].IParameterField
+        Get
+            Return Me.DataDefinition.ParameterFields(0)
+        End Get
+    End Property
 End Class
 
 <System.Drawing.ToolboxBitmapAttribute(GetType(CrystalDecisions.[Shared].ExportOptions), "report.bmp")>  _
